16f54ef2c4dce4458ca59cfbbc8e2c92893dd6f9
Mark-Alexis-Posadas <67795586+Mark-Alexis-Posadas@users.noreply.github.com>
2026-07-27 11:46:30 +0800

    update: pagination js

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,18 +1,19 @@
 @props(['headers' => []])
-
-<div class="table-responsive shadow-sm rounded">
-    <table class="table table-striped mb-0">
-        <thead class="table-dark">
-            <tr>
-                @foreach($headers as $header)
-                    <th @if($header === 'Action') class="text-center" width="180" @endif>
-                        {{ $header }}
-                    </th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody>
-            {{ $slot }}
-        </tbody>
-    </table>
+<div id="table-container">
+    <div class="table-responsive shadow-sm rounded">
+        <table class="table table-striped mb-0">
+            <thead class="table-dark">
+                <tr>
+                    @foreach ($headers as $header)
+                        <th @if ($header === 'Action') class="text-center" width="180" @endif>
+                            {{ $header }}
+                        </th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                {{ $slot }}
+            </tbody>
+        </table>
+    </div>
 </div>
